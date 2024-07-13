981ccea179195981200dbc36cd9676760089649b
Rit53 <ritesh.kumar@liet.in>
2024-07-13 18:17:11 +0000

    conversion of data type

---
+++ javaScript/conversion.js
@@ -0,0 +1,26 @@
+let score = 33
+
+console.log(typeof(score))
+
+let scoreInString=String(score); // type conversion number to string
+
+console.log(typeof(scoreInString))
+
+// "33" =>33
+// "33abc" => NaN
+// true => 1; false=>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "ritesh" => true
+
+let num= "22"
+
+let val=Number(num)
+console.log(val)
+console.log(typeof(val))
