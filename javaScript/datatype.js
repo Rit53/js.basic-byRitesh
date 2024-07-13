06f50082924d92bac5112da28fa4036d2cf1afbe
Rit53 <ritesh.kumar@liet.in>
2024-07-13 17:23:57 +0000

    datatype

---
+++ javaScript/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using nodejs , not browser
+
+let name="ritesh"
+let age=18
+let isLoggedIn=false
+
+let state;
+
+
+// number => 2 to power 53
+// bigint
+//strng => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol
+
+
+//Object
+
+console.log(typeof null); // data type object
+console.log(typeof undefined); // undefined
